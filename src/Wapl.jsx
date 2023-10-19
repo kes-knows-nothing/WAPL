5f13a9ee874d3a306203b299c2ff3eb94684a868
kes-knows-nothing <allinpositive@gmail.com>
2023-10-19 14:01:03 +0900

    wapl 소개 수정

--- src/Wapl.jsx
+++ src/Wapl.jsx
@@ -17,7 +17,7 @@ const Wapl = () => {
         title="Embedded Figma Document"
       />
       <span className="information_line">WPAL은 메신저형 그리고 업무형 협업 두 가지 형태를 종합한 올인원 협업 툴을 지향한다고 생각합니다.</span>
-      <span className="information_line">비단, 칸반 보드와 메신저 기능 구현을 넘어 <span className='highlight'>새로운 창으로 이동하지 않고 어플리케이션 내에서 동작</span>한다는 것이 큰 특징입니다.</span>
+      <span className="information_line"><span className='highlight'>모든 기능이 새로운 창으로 이동하지 않고 어플리케이션 내에서 동작</span>한다는 것이 큰 특징입니다.</span>
       <span className="information_line">더불어, 메일 시스템도 내장시켜 통합적인 시스템 구축 1선에 있다고 생각합니다.</span>
     </div>
   );
